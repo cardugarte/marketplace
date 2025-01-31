117ba827c400df29835619cce3b7b928d7a93a2d
Carlos Dugarte <soycarlosdugarte@gmail.com>
2025-01-31 19:48:57 -0300

    Se crean las primeras paginas de la aplicación

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'SEO Title',
+  description: 'SEO Description',
+};
+
+
+export default function About() {
+  return (
+    <>
+      <h2>About Page</h2>
+    </>
+  )
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+
+export const metadata = {
+  title: 'SEO Title',
+  description: 'SEO Title',
+};
+
+
+export default function Contact() {
+  return (
+    <>
+      <h2>Contact Page</h2>
+    </>
+  )
+}
