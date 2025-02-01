ac64931a621c2dd01e66948afd2bad802213e3c3
Carlos Dugarte <soycarlosdugarte@gmail.com>
2025-02-01 16:34:23 -0300

    add dockerfile configurations

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  //active with dockerfile is executable:
+  //output: "standalone"
 };
 
 export default nextConfig;
