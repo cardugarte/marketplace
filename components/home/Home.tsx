e682cb82a76f4489386bd6977c36b9764b54dc13
Carlos Dugarte <soycarlosdugarte@gmail.com>
2025-02-12 19:27:49 -0300

    add new configurations and dashboard page

--- components/home/Home.tsx
+++ components/home/Home.tsx
@@ -14,6 +14,8 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel"
+import { Input } from '@/components/ui/input'
+import Link from 'next/link';
 
 
 
@@ -23,7 +25,7 @@ export const HomePage = () => {
     Autoplay({ delay: 2000, stopOnInteraction: true })
   )
   return (
-    <>
+    <div className='h-full w-full p-2 flex flex-col'>
       <div className="grid items-center justify-items-end h-full p-8 pb-1 gap-16 sm:p-1 font-[family-name:var(--font-geist-sans)]">
         <Navbar />
       </div>
@@ -32,8 +34,11 @@ export const HomePage = () => {
           <BitcoinCircleIcon style={{ height: "150px", width: "150px", color: '#FFFFFF' }} />
         </div>
         <h2 className="text-2xl">A simple and powerful platform to explore Argentina by paying with Bitcoin.</h2>
-        <div className='mt-10 mb-10'>
-          <Button variant={'secondary'} > Discovery your next adventure </Button>
+        <div className='mt-10 mb-10 w-full flex flex-row items-center justify-center align-middle md:flex-col'>
+          <Input className='max-w-96 mr-2' placeholder='What do you have in mind?' />
+          <Button className='max-w-40 lg:mt-2' variant={'secondary'} >
+            <Link href={'/dashboard'}>Discovery</Link>
+          </Button>
         </div>
         <div className='flex items-center justify-center'>
           <Carousel
@@ -60,6 +65,9 @@ export const HomePage = () => {
           </Carousel>
         </div>
       </main>
-    </>
+      <footer className='text-center pt-20'>
+        <h2>This platform is created with love by Carlit0s🍷</h2>
+      </footer>
+    </div>
   )
 }
--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -31,7 +31,6 @@ export const Navbar = () => {
         </MenubarContent>
       </MenubarMenu>
     </Menubar>
-
     // </>
   )
 }
